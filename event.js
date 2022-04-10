883715fd19d3326dcce756cfd1a8bfbef515cb5e
smrutipohekar17 <95218732+smrutipohekar17@users.noreply.github.com>
2022-04-10 19:52:48 +0530

    Update event.js

--- event.js
+++ event.js
@@ -1,1 +1,44 @@
-let nav= document.querySelector;
+let menu= document.querySelector('#menu-bars');
+let navbar= document.querySelector('.navbar');
+
+menu.onclick= () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+window.onscroll = ()=>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  themeToggler.classList.remove('active');
+}
+let themeToggler = document.querySelector('.theme-toggler');
+let toggleBtn = document.querySelector('.toggle-btn');
+
+
+toggleBtn.onclick = ()=>{
+  themeToggler.classList.toggle('active');
+
+}
+document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn =>{
+  btn.onclick= () =>{
+    let color =btn.style.background;
+    document.querySelector(':root').style.setProperty('--main-color', color);
+  }
+});
+var swiper = new Swiper(".home-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop:true,
+  autoplay:{
+    delay:2000,
+    disableOnInteraction:false,
+  }
+});
